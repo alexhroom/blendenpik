c94a9c0630a28b99f62b997f0305e6f875a0ca72
alexhroom <alex.room@btinternet.com>
2026-01-19 14:29:04 +0000

    finished up

--- blendenpik.f90
+++ blendenpik.f90
@@ -1,61 +1,71 @@
 ! Blendenpik sketch-and-precondition algorithm.
 
 module blendenpik_module
-  implicit none (external)
+  implicit none (type)
+
+  external dfftw_plan_r2r_1d, dfftw_execute_dft, dfftw_destroy_plan, FFTW_REDFT10, FFTW_ESTIMATE
+  external dgeqrf, dgels, dtrtrs
+
   private
   public :: blendenpik
 
 contains
-  subroutine blendenpik(A, b, x, s)
+  subroutine blendenpik(A, b, x, m, n, s)
     ! Solves the linear system Ax = b using the Blendenpik algorithm.
     ! A: Input matrix (m x n)
     ! b: Right-hand side vector (m)
     ! x: Solution vector (n)
     ! s: Sketch size
 
-    real(real64), intent(in) :: A(:,:), b(:)
-    real(real64), intent(out) :: x(:)
-    integer, intent(in) :: s
+    real(8), intent(in) :: A(:,:), b(:)
+    real(8), intent(out) :: x(:)
+    integer, intent(in) :: s, m, n
 
-    integer :: m, n, i, info, lwork
+    integer :: i, info, lwork, plan
+
+    real(8) :: rand_no
     integer, dimension(s) :: row_indices
-    real(real64), dimension(m, n) :: M
-    real(real64), dimension(m, m) :: D
-    real(real64), dimension(s, n) :: SM
-    real(real64) :: R(n, n)
+    real(8), dimension(s) :: harvest
+    real(8) :: work_query(1)
+    real(8), dimension(m, n) :: DCT_M, tau
+    real(8), dimension(s, n) :: SM
+    real(8), dimension(n, n) :: R
+    real(8), allocatable :: work(:)
 
-    m = size(A, 1)
-    n = size(A, 2)
 
-    ! create diagonal matrix D with random ±1 entries
-    real(real64) :: D(:)
-    real(real64) :: diag(n)
-    D = 0.0d0
-    where (diag > 0.5d0)
-      diag = 1.0d0
-    elsewhere
-      diag = -1.0d0
-    end where
+    ! create DA, which is A with random sign flips
+    ! i.e. D is a random diagonal matrix with +/- 1 on the diagonal
+    call random_seed()
     do i = 1, m
-      D(i, i) = diag(i)
+      call random_number(rand_no)
+      if (rand_no < 0.5d0) then
+        DCT_M(i, :) = -A(i, :)
+      else
+        DCT_M(i, :) = A(i, :)
+      end if
     end do
 
-    ! compute M which is the DCT of DA
-    call dgemm('N', 'N', m, n, n, 1.0d0, D, m, A, n, 0.0d0, M, m)
-
-    !call dct(......)
-
+    ! apply discrete cosine transform to each column of DA using fftw
+    call dfftw_plan_r2r_1d(plan, m, DCT_M(:, 1), DCT_M(:, 1), FFTW_REDFT10, FFTW_ESTIMATE)
+    do i = 1, n
+      call dfftw_execute_dft(plan, DCT_M(:, i), DCT_M(:, i))
+    end do 
+    call dfftw_destroy_plan(plan)
+    
     ! sketch by selecting s random rows of M
-    call random_seed()
-    call random_number(row_indices)
+    call random_number(harvest)
     row_indices = mod(int(row_indices * m), m) + 1
     do i = 1, s
-      SM(i, :) = M(row_indices(i), :)
+      SM(i, :) = DCT_M(row_indices(i), :)
     end do
 
     ! now take QR decomposition of SM
-    ! do workspace stuff...
+    ! workspace query
+    call dgeqrf(s, n, SM, s, tau, work_query, -1, info)
+    lwork = int(work_query(1))
+    allocate(work(lwork))
     call dgeqrf(s, n, SM, s, tau, work, lwork, info)
+    deallocate(work)
 
     ! extract R from QR decomposition
     R = 0.0d0
@@ -63,7 +73,16 @@ contains
       R(i, i:n) = SM(i, i:n)
     end do
 
-    ! then do LSQR to solve the preconditioned system
-    ! and then recover x
+    ! now solve the preconditioned least squares problem
+    ! i.e. minimize ||A * R^{-1} y - b||_2
+    ! then set x = R^{-1} y
+    ! workspace query
+    call dgels('N', m, n, 1, A, m, b, m, x, n, work_query, -1, info)
+    lwork = int(work_query(1))
+    allocate(work(lwork))
+    call dgels('N', m, n, 1, A, m, b, m, x, n, work, lwork, info)
+    call dtrtrs('U', 'N', 'N', n, 1, R, n, x, n, info)
+    deallocate(work)
+
   end subroutine blendenpik
-  end module blendenpik_module
+end module blendenpik_module
